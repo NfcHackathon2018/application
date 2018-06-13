242a6aa22e519d883e966e42bdfa15e87294f63d
HikaruSakurada <>
2018-06-14 00:55:36 +0900

    initial commit

---
+++ app/src/main/java/omamori/nfchackathon/co/jp/omamori/SplashActivity.java
@@ -0,0 +1,13 @@
+package omamori.nfchackathon.co.jp.omamori;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_splash);
+    }
+}
